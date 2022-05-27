4ae716a15bfa2594bd29c13cf255c5dd46257762
Julian ranieri <julianranieri@Julians-MBP.lan>
2022-05-26 18:19:53 -0600

    working on categories

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 // INIT
+
+// header
+$('?header').innerHTML = /*html*/ `
+  <img src="/assets/flowers1edit.webp" style="width: 140px;">
+  <p>Tabitha Ministries</p>
+  <i id="hamburger" style="font-size: 30px; cursor: pointer;" class="fa fa-bars mobile-nav"></i>
+`
+
+// nav
+$('?nav').innerHTML = /*html*/ `
+  <div>
+  <a href="/index.html">Home</a>
+  <a href="">About</a>
+  <a href="/categories.html">Categories</a>
+  <a href="">Recent</a>
+  </div>
+`
+
+// footer
+$('?footer').innerHTML = /*html*/ `
+  <p>Contact me etc etc...</p>
+  <a href="">Privacy Policy or Whatever</a>
+`
+
+// Globals
 var postOffset = 0
 var numberOfPosts = 0
+var currentLocation = window.location.pathname.split('?')[0]
+// on page loads
+switch(currentLocation) {
+  case '/index.html':
+    getPost()
+    break
+  case '/categories.html':
+    getCategories()
+    break
+  case '/category.html':
+    getPostsbyCategory()
+    break
+}
 
-if(window.location.pathname === '/index.html') getPost()
+// if(window.location.pathname === '/index.html') getPost()
 
 /////////////// POSTS ///////////////
 async function getPost() {
@@ -15,19 +53,38 @@ async function getPost() {
     }
     spinner(false)
     var data = await res.json()
+    console.log(data)
     // set number of posts
     numberOfPosts = data.found
     // format post date
     var dataDate = data.posts[0].date
-    let formattedDate = getFormattedDate(dataDate)
+    var formattedDate = getFormattedDate(dataDate)
+    // get categories for post
+    var categoriesData = Object.values(data.posts[0].categories)
+    // format html
     var html = /*html*/ `
       <section>
         <h2>${data.posts[0].title}</h2>
         <small class="date-time">${formattedDate}</small>
+        <small id="categories-list">Categories: </small>
         <div>${data.posts[0].content}</div>
       </section>
     `
     $('#latest-post').innerHTML = html
+    // set categories list
+    categoriesData.forEach(category => {
+      if(category.name !== "Uncategorized") {
+        var element = document.createElement('a')
+        element.innerHTML = `[${category.name}]`
+        element.href = `/category.html?title=${category.name}`
+        element.style.marginLeft = '10px'
+        $('#categories-list').appendChild(element)
+      }
+    })
+    // remove categories if none
+    if($('#categories-list').children.length === 0) {
+      $('#categories-list').remove()
+    }
   } catch(err) {
     spinner(false)
     console.log(err)
@@ -36,17 +93,73 @@ async function getPost() {
   }
 }
 
-function handlePreviousPost() {
-  if(postOffset !== 0) {
-    postOffset -= 1
-    getPost()
+// get posts by category
+async function getPostsbyCategory() {
+  var category = window.location.search.split('=')[1]
+  try {
+    spinner(true)
+    var res = await fetch(`https://public-api.wordpress.com/rest/v1.1/sites/tabithaministriesmt.wordpress.com/posts/?category=${category}&number=1&offset=${postOffset}`)
+    if(!res.ok) {
+      throw new Error(res.status)
+    }
+    spinner(false)
+    var data = await res.json()
+    console.log(data)
+    data.posts.forEach(post => {
+      var element = document.createElement('section')
+      // format post date
+      var formattedDate = getFormattedDate(post.date)
+      // format html
+      var html = /*html*/ `
+        <h2>${post.title}</h2>
+        <small class="date-time">${formattedDate}</small>
+        <div>${post.content}</div>
+      `
+      element.style.marginBottom = '20px'
+      element.innerHTML = html
+      $('#posts').appendChild(element)
+    })
+
+  } catch(err) {
+    spinner(false)
+    console.log(err)
   }
 }
 
-function handleNextPost() {
+function handlePrevious(mod, callback) {
+  if(postOffset !== 0) {
+    postOffset -= mod
+    callback()
+  }
+}
+
+function handleNext(mod, callback) {
   if(postOffset === numberOfPosts - 1) return
-  postOffset += 1
-  getPost()
+  postOffset += mod
+  callback()
+}
+
+// get Categories
+async function getCategories() {
+  try {
+    spinner(true)
+    var res = await fetch('https://public-api.wordpress.com/rest/v1.1/sites/tabithaministriesmt.wordpress.com/categories')
+    if(!res.ok) {
+      throw new Error(res.status)
+    }
+    spinner(false)
+    var data = await res.json()
+    console.log(data)
+    data.categories.forEach(category => {
+      var element = document.createElement('h2')
+      element.innerHTML = /*html*/ `<a href="/category.html?title=${category.name}">${category.name}</a>`
+      $('#categories').appendChild(element)
+    })
+
+  } catch(err) {
+    spinner(false)
+    console.log(err)
+  }
 }
 
 //////////////////// Utilities /////////////////////////////
